0a4b8cd0ad76911444c9478d4c6fd002421e8875
r00ckky <chaitkohli10@gmail.com>
2024-01-30 14:23:36 +0530

    Basic chatbot with knowledge graph

--- Chatbot.py
+++ Chatbot.py
@@ -1,25 +1,34 @@
 import os
+import langchain
 from langchain_openai import ChatOpenAI
 from langchain import PromptTemplate
 from langchain.memory import ConversationBufferMemory
-# from langchain.prompts import (
-#     ChatPromptTemplate,
-#     MessagesPlaceholder,
-#     SystemPromptTemplate,
-#     HumanMessagePromptTemplate
-# )
-from langchain.chains.conversation.memory import (
-    ConversationBufferMemory,
-    ConversationSummaryMemory,
-    ConversationBufferWindowMemory,
-    ConversationKGMemory
-)
-
+from langchain.chains.conversation.memory import ConversationKGMemory
 from langchain_community.callbacks import get_openai_callback
-
+from langchain.chains import LLMChain, ConversationChain
 from dotenv import load_dotenv
 
 load_dotenv()
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
+class LLM_Chain:
+    def __init__(self, openai_api_key, memory:str, model:str='gpt-3.5-turbo', temperature=0.7):
+        self.chatbot = ChatOpenAI(
+            model = model,
+            temperature = temperature,
+            openai_api_key = openai_api_key,
+        )
 
+        self.chain = ConversationChain(
+            llm = self.chatbot,
+            memory = ConversationKGMemory(
+                llm=self.chatbot,
+            ),
+        )
+
+    def generate_response(self, query):
+        with get_openai_callback() as cb:
+            result = self.chain.run(query)
+            return result, cb.total_tokens
+        
+        
