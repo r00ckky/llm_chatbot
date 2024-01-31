dfa4d71088dc3a1522df546faa7941ca2c223589
Vaibhav Sharma <vaibhavsharmsk125@gmail.com>
2024-01-31 16:29:25 +0530

    Make changes

--- Speech.py
+++ Speech.py
@@ -1,14 +1,8 @@
 import speech_recognition as sr
-import pyttsx3
+
 r = sr.Recognizer()
 
-def SpeakText(command):
-    engine = pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
-    
-    
-while(1):
+while True:
     try:
         with sr.Microphone() as source2:
             r.adjust_for_ambient_noise(source2, duration=0.2)
@@ -17,9 +11,14 @@ while(1):
             MyText = r.recognize_google(audio2)
             MyText = MyText.lower()
             
-            print("Did you say ",MyText)
-            SpeakText(MyText)
+            print("Did you say: ", MyText)
+        
+        # Break out of the loop after successful recognition
+        break
+
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
+        break
+
     except sr.UnknownValueError:
-        print("Unknown error occurred")
+        print("Unknown error occurred. Please try again.")
