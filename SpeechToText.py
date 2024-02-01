a4846e3601961310b8aa0a66701e11f0f27ff315
Shivansh-Btgr <shivanshbtgr25@gmail.com>
2024-02-01 15:49:01 +0530

    Added Recording and Transcribing of Audio

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,5 +1,40 @@
 import whisper
+import pyaudio
+import wave
 
+#capturing Audio till keyboard interrupts
+def CapAudio() :
+
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format = pyaudio.paInt16, channels=1, rate=44100, input = True, frames_per_buffer = 1024)
+
+    frames = []
+
+    try :
+        while True:
+            data = stream.read(1024)
+            frames.append(data)
+
+    except KeyboardInterrupt:
+        pass
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    #Creating The Audio File
+    sound_file = wave.open("myaud.wav","wb")
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+    sound_file.setframerate(44100)
+
+    sound_file.writeframes(b''.join(frames))
+    sound_file.close()
+
+    #File stored in a var
+    audFile = 'myaud.wav'
+
+#Transcribing The Audio
 def TranscribeAud(Aud) :
 
     model = whisper.load_model("base")
