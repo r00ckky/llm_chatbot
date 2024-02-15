9788262d51b050fd8187f3ee47e4eebbd30100e0
r00ckky <chaitkohli10@gmail.com>
2024-02-15 19:36:59 +0530

    removing scripts and renaming them

--- tts_test.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-import scipy
-
-synthesiser = pipeline("conversational", "suno/bark-small")
-
-speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"do_sample": True})
-
-scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])
