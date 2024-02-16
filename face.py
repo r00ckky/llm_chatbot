565f26df6ca405361aa23fb9814871a65edffd40
r00ckky <chaitkohli10@gmail.com>
2024-02-16 15:01:25 +0530

    one-shot implemented

--- face.py
+++ face.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import os
 from uuid import uuid4
+
 class Face:
     def __init__(self, DIR_FACES:str=None) -> None:
         self.dir = DIR_FACES if DIR_FACES is not None else 'face'
@@ -23,30 +24,52 @@ class Face:
             top, right, bottom, left = max_area_tuple
             face_image = image[top:bottom, left:right]
             face_encodings = fr.face_encodings(image, [max_area_tuple])
-            match = fr.compare_faces(self.known_face_encoding, face_encodings)
-        else:
-            face_encodings = fr.face_encodings(image, face_locations)
-            for face_encoding in face_encodings:
-                match = fr.compare_faces(self.known_face_encoding, face_encoding)
+            match = [fr.compare_faces(self.known_face_encoding, face_encoding)[0] for face_encoding in face_encodings]
             if True in match:
                 index = match.index(True)
                 name = self.face_name[index]
-                return name, face_image
-        
-        if name is None:
-            name = uuid4()
+            else:
+                name = str(uuid4())
+            self.save_face_info(name, face_image)
             return name, face_image
         
-        def save_face_info(self, name:str, face_image:np.array):
-            self.face_name.append(name)
-            self.known_face.append()
+        else:
+            names = []
+            face_images = []
+            face_encodings = fr.face_encodings(image, face_locations)
+            for face_encoding, face_loc in zip(face_encodings, face_locations):
+                matches = fr.compare_faces(self.known_face_encoding, face_encoding)
+                top, right, bottom, left = face_loc
+                face_image = image[top:bottom, left:right]
+                
+                face_images.append(image[top:bottom, left:right])
+                if True in matches:
+                    first_match_index = matches.index(True)
+                    name = self.face_name[first_match_index]
+                    names.append(self.face_name[first_match_index])
+                else: 
+                    name = str(uuid4())
+                    names.append(name)
+                self.save_face_info(name, face_image)
+            return names, face_images
+        
+    def save_face_info(self, names, face_images):
+        if type(name)==list and type(face_image)==list:
+            for name, face_image in zip(names, face_images):
+                self.face_name.append(name)
+                self.known_face.append(face_image)
+                self.known_face_encoding(fr.face_encodings(face_image)[0])
+        else:    
+            self.face_name.append(names)
+            self.known_face.append(face_images)
+            self.known_face_encoding(fr.face_encodings(face_images)[0])
 
 if __name__ == '__main__':
     bruh = Face(DIR_FACES='face')
     img = cv2.imread('people.jpeg')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    print(img.shape)
-    name, face_image = bruh.get_face_info(img, True)
-    print(name, face_image.shape)
-    cv2.imwrite(f'{name}.jpeg',face_image)
+    names, face_images = bruh.get_face_info(img, False)
+    print(names)
+    for name, face_image in zip(names, face_images):
+        cv2.imwrite(f'{name}.jpeg', face_image)
     
